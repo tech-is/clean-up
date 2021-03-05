4844620af4c0644553facdcdc332af4de67076fb
桐影 <kirikage@MacBook-Pro.local>
2021-03-05 20:59:04 +0900

    テストファイルを追加

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>片付け管理システム</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <link href="http://localhost/teamIg/assets/css/clip.css" rel="stylesheet" type="text/css" />
+</head>
+
+<div id="header">
+<img src="img/techis-logo.svg" alt="タイトルロゴ" title="片付け管理システム" class="logo">
+<div style="padding-top:20px;">
+<button type="button" class="btn btn-success mt-1 update_botton">更新</button>
+    <p class="abc user_data">
+        ユーザーID:
+        <font color=#f00>*user[id]*aaaaaa<br></font>
+        登録者名:  
+        <font color=#f00>*user[name]*aaaa</p><br></font>
+  </div>
+</div>
