3b1801cc4043fb62828564ee00aceb90559edb0f
takezi <takezi@users.noreply.github.com>
2021-03-20 21:25:26 +0900

    モデルの修正

--- Codeigniter/application/models/item_model.php
+++ Codeigniter/application/models/item_model.php
@@ -7,34 +7,35 @@ class Item_model extends CI_Model
         $this->load->database();
     }
 
-    public function find($id, $user_id, $place_id)
+    public function find($id)
     {
         return $this->db->where('id', $id)
             ->get('item')
             ->row_array();
     }
 
-    public function findAll($user_id, $place_id)
+    public function findAll($user_id)
     {
-        return $this->db->get('item')
+        return $this->db->where('user_id', $user_id)
+            ->get('item')
             ->result_array();
     }
 
-    public function insert($data, $user_id, $place_id)
+    public function insert($data)
     {
-        $this->db->insert('item', $data, $user_id, $place_id);
+        $this->db->insert('item', $data);
         return $this->db->insert_id();
     }
 
-    public function update($id, $user_id, $place_id, $data)
+    public function update($id, $data)
     {
         $this->db->where('id', $id)
-            ->update('user', $data, $user_id, $place_id);
+            ->update('item', $data);
     }
 
-    public function delete($id, $user_id, $place_id)
+    public function delete($id)
     {
-        $this->db->where('id', $id, $user_id, $place_id)
-            ->delete();
+        $this->db->where('id', $id)
+            ->delete('item');
     }
 }
